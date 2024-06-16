79b64031078f210ee57ca6bde9d6a4bb1e7f6f1f
ppp3ppj <ppp.3ppj@gmail.com>
2024-06-16 16:40:38 +0700

    config tailwind and build js example.

---
+++ dev.js
@@ -0,0 +1,15 @@
+const esbuild = require("esbuild");
+
+esbuild
+  .build({
+    entryPoints: ["./static/script/*.js"],
+    bundle: true,
+    minify: true,
+    treeShaking: true,
+    outdir: "./dist",
+    target: ["chrome58", "firefox57", "safari11"],
+  })
+  .then(() => console.log("⚡[Dev] - Bundle build complete ⚡"))
+  .catch(() => {
+    process.exit(1);
+  });
