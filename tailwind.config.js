867cc32bfb89d4fbe07dec8dca3241fb203fbb97
ppp3ppj <ppp.3ppj@gmail.com>
2024-06-16 17:09:35 +0700

    add htmx and hyperscript

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,77 @@
-/** @type {import('tailwindcss').Config} */
+import { screens as _screens } from 'tailwindcss/defaultTheme';
+import plugin from 'tailwindcss/plugin';
+
+export const darkMode = 'class';
+// example content path templ and *js
+export const content = ["./pub/**/*.templ", "./src/**/*.js"];
+export const theme = {
+    screens: {
+      'xs': '320px',
+      'sm': '640px',
+      'md': '768px',
+      'itn': '880px',
+      'lg': '1024px',
+      'itn2':'1150px',
+      'xl': '1280px',
+      '2xl': '1536px',
+      '3xl': '1920px',
+      '4xl': '2560px',
+    },
+    extend: {},
+};
+export const plugins = [
+    plugin(function({ addBase }) {
+        addBase({
+            'html': {
+                fontSize: "16px", // Default font size, which is typically 16px
+                '@screen md': {
+                    fontSize: "14px", // Font size on medium (md) breakpoint, which is typically 14px
+                },
+            },
+        });
+    }),
+    require('@tailwindcss/typography'),
+    require('daisyui')
+];
+export const daisyui = {
+    themes: [
+        "cupcake",
+        "sunset",
+    ],
+    darkTheme: "sunset",
+};
+
+/**
 module.exports = {
-  content: [],
+  content: [
+  ],
   theme: {
+    screens: {
+      'xs': '320px',
+      'sm': '640px',
+      'md': '768px',
+      'itn': '880px',
+      'lg': '1024px',
+      'itn2':'1150px',
+      'xl': '1280px',
+      '2xl': '1536px',
+      '3xl': '1920px',
+      '4xl': '2560px',
+    },
     extend: {},
   },
-  plugins: [],
+  plugins: [
+   plugin(function({ addBase }) {
+        addBase({
+            'html': {
+                fontSize: "16px", // Default font size, which is typically 16px
+                '@screen md': {
+                    fontSize: "14px", // Font size on medium (md) breakpoint, which is typically 14px
+                },
+            },
+        });
+    }),
+    require('daisyui')
+  ],
 }
-
+*/
