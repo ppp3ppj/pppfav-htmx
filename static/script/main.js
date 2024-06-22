b412441262aac9c4f239ba4274cfe983a2b27520
ppp3ppj <ppp.3ppj@gmail.com>
2024-06-22 23:09:01 +0700

    fix when change theme not change to default theme

--- static/script/main.js
+++ static/script/main.js
@@ -5,7 +5,8 @@ _hyperscript.browserInit()
 const colorSchemeQuery = window.matchMedia('(prefers-color-scheme: dark)');
 
 colorSchemeQuery.addEventListener('change', () => {
-   window.initialState()
+
+  window.initialState()
 });
 
 window.addEventListener('load', () => {
@@ -16,52 +17,56 @@ window.addEventListener('load', () => {
 });
 
 window.initialState = function() {
-   const darkToggle = document.getElementById('dark-toggle')
-   if (darkToggle) {
+  const darkToggle = document.getElementById('dark-toggle')
+  if (darkToggle) {
+    if (localStorage.getItem('dark-mode') === null || localStorage.getItem('dark-mode') === undefined) {
       darkToggle.checked = !colorSchemeQuery.matches
-   }
-   const darkToggleMobile = document.getElementById('dark-toggle-mobile')
-   if (darkToggleMobile) {
-      darkToggle.checked = !colorSchemeQuery.matches
-   }
-
-   window.toggleDarkMode(darkToggle.checked)
-   window.setMkTheme(null, null)
+    } else {
+      darkToggle.checked = !(localStorage.getItem('dark-mode') === 'true')
+    }
+  }
+  window.toggleDarkMode(darkToggle.checked, darkToggle.checked === !colorSchemeQuery.matches)
+  window.setMkTheme(null, !darkToggle.checked)
 }
 
-window.toggleDarkMode = function(isChecked) {
-   if (!isChecked) {
-      document.documentElement.classList.add("dark")
-      document.documentElement.classList.remove("light")
-   } else {
-      document.documentElement.classList.remove("dark")
-      document.documentElement.classList.add("light")
-   }
+window.toggleDarkMode = function(isChecked, save = true) {
+  if (!isChecked) {
+    document.documentElement.classList.add("dark")
+    document.documentElement.classList.remove("light")
+  } else {
+    document.documentElement.classList.remove("dark")
+    document.documentElement.classList.add("light")
+  }
+
+  if (save && (isChecked === !colorSchemeQuery.matches)) {
+    localStorage.removeItem('dark-mode')
+  } else if (save){
+    localStorage.setItem('dark-mode', !isChecked)
+  }
 }
 
 window.setMkTheme = function(theme, isDark) {
-   console.log(isDark)
-   if (isDark === null) {
-      isDark = colorSchemeQuery.matches
-   }
-   if (isDark) {
+  if (isDark === null) {
+    isDark = colorSchemeQuery.matches
+  }
+  if (isDark) {
+    if (theme === null || theme === undefined) {
+      theme = localStorage.getItem('mk-theme-dark')
       if (theme === null || theme === undefined) {
-         theme = localStorage.getItem('mk-theme-dark')
-         if (theme === null || theme === undefined) {
-            theme = 'sunset'
-         }
+        theme = 'sunset'
       }
-      localStorage.setItem('mk-theme-dark', theme)
-   } else {
+    }
+    localStorage.setItem('mk-theme-dark', theme)
+  } else {
+    if (theme === null || theme === undefined) {
+      theme = localStorage.getItem('mk-theme-light')
       if (theme === null || theme === undefined) {
-         theme = localStorage.getItem('mk-theme-light')
-         if (theme === null || theme === undefined) {
-            theme = 'cupcake'
-         }
+        theme = 'cupcake'
       }
-      localStorage.setItem('mk-theme-light', theme)
-   }
+    }
+    localStorage.setItem('mk-theme-light', theme)
+  }
 
-   const baseElement = document.documentElement;
-   baseElement.setAttribute('data-theme', theme)
+  const baseElement = document.documentElement;
+  baseElement.setAttribute('data-theme', theme)
 }
