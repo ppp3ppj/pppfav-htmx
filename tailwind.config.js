3da23a1e3e64662f30caed8fb6d8fe3e2b75d48e
ppp3ppj <ppp.3ppj@gmail.com>
2024-06-23 16:58:01 +0700

    remove unuse comment code

--- tailwind.config.js
+++ tailwind.config.js
@@ -50,38 +50,3 @@ export const daisyui = {
     ],
     darkTheme: "sunset",
 };
-
-/**
-module.exports = {
-  content: [
-  ],
-  theme: {
-    screens: {
-      'xs': '320px',
-      'sm': '640px',
-      'md': '768px',
-      'itn': '880px',
-      'lg': '1024px',
-      'itn2':'1150px',
-      'xl': '1280px',
-      '2xl': '1536px',
-      '3xl': '1920px',
-      '4xl': '2560px',
-    },
-    extend: {},
-  },
-  plugins: [
-   plugin(function({ addBase }) {
-        addBase({
-            'html': {
-                fontSize: "16px", // Default font size, which is typically 16px
-                '@screen md': {
-                    fontSize: "14px", // Font size on medium (md) breakpoint, which is typically 14px
-                },
-            },
-        });
-    }),
-    require('daisyui')
-  ],
-}
-*/
