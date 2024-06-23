ae8c50e5c660a823ff973069def35a07cfd2cb8b
ppp3ppj <ppp.3ppj@gmail.com>
2024-06-23 16:29:05 +0700

    add util ternary compare

---
+++ utils/ternary/ternary.go
@@ -0,0 +1,49 @@
+// Package ternary implements ternary-like operator
+package ternary
+
+// String returns non nil value if available
+func String(a, b string) string {
+	if len(a) > 0 {
+		return a
+	}
+	return b
+}
+
+func Struct[T any](a, b *T) *T {
+	if a != nil {
+		return a
+	}
+	return b
+}
+
+// Uint returns non nil value if available
+func Uint(a, b uint) uint {
+	if a > 0 {
+		return a
+	}
+	return b
+}
+
+// Int returns non nil value if available
+func Int(a, b int) int {
+	if a > 0 {
+		return a
+	}
+	return b
+}
+
+// Min returns the smaller of a or b
+func Min(a, b int) int {
+	if a > b {
+		return b
+	}
+	return a
+}
+
+// Max returns the larger of a or b
+func Max(a, b int) int {
+	if a < b {
+		return b
+	}
+	return a
+}
